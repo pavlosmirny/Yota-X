9ed0a30b14c4a07247d60b45713ab60fa63a1f89
pavlosmirny <pavlo.smirnyi@yota-x.com>
2025-01-09 14:24:51 +0200

    fix 2

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/sitemap.xml",
-        destination: "/api/sitemap", // Аналогично для sitemap, если потребуется
+        destination: "/api/sitemap.xml", // Аналогично для sitemap, если потребуется
       },
       // Добавьте другие правила при необходимости
     ];
